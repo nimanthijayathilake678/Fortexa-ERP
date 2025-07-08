2a12491132aeef23ff0766c87f4d7740da83587c
nimanthiJ <nimanthij@ewisl.net>
2025-07-08 14:31:56 +0530

    USER_Service:Implimentations in UserService

--- user-service/backend/src/main/java/com/shop/microservices/user_service/Service/Serviceinterface/IUserService.java
+++ user-service/backend/src/main/java/com/shop/microservices/user_service/Service/Serviceinterface/IUserService.java
@@ -1,4 +1,9 @@
 package com.shop.microservices.user_service.Service.Serviceinterface;
 
+import com.shop.microservices.user_service.Dto.UserRequestDTO;
+import com.shop.microservices.user_service.Dto.UserResponseDTO;
+
 public interface IUserService {
+    //add new user
+    UserResponseDTO AddNewUser(UserRequestDTO userRequestDTO);
 }
